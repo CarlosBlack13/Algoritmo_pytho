f22964225e67e40743d866c89c81c7f9d0ca27ea
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2025-04-03 20:23:02 -0300

    Add files via upload

---
+++ temperaturaf-10.py
@@ -0,0 +1,5 @@
+temperatura_celsius = float(input("Informe a temperatura em graus Celsius: "))
+
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+print(f"A temperatura em graus Fahrenheit é: {temperatura_fahrenheit:.2f}°F")
